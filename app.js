affbe2023780860a8638c14cf48aa48150c20d55
Alejandro <alejandro.quinones@outlook.es>
2022-06-18 12:22:01 -0400

    primerBorrador

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*------------ Decoracion------- */
+
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
 const img3 = document.getElementById('img3');
@@ -5,19 +7,26 @@ const img3 = document.getElementById('img3');
 const previous = document.querySelector('.previous');
 const next = document.querySelector('.next');
 const current = document.querySelector('.current');
-const box = document.querySelector('.box');
-const p = document.querySelector('.p');
-const theCount = document.querySelector('.theCount');
-const less = document.querySelector('.less');
-const currentNumber = document.querySelector('.currentNumber');
-const more = document.querySelector('.more');
+const alertaM = document.querySelector('.alertaM');
+const alertaI = document.querySelector('.alertaI');
+const alertaG = document.querySelector('.alertaG');
+
+
+const boxMartian = document.querySelector('.boxMartian');
+const boxInter = document.querySelector('.boxInter');
+const boxGrav = document.querySelector('.boxGrav');
+
+const lessM = document.querySelector('.lessM');
+const moreM = document.querySelector('.moreM');
+
+const lessI = document.querySelector('.lessI');
+const moreI = document.querySelector('.moreI');
+
+const lessG = document.querySelector('.lessG');
+const moreG = document.querySelector('.moreG');
 
 
 let contador = 2;
-
-let stickerMR = 0;
-let stickerIn = 0;
-let stickerGrav = 0;
 
 
 const slider = () => {
@@ -53,98 +62,195 @@ const slider = () => {
     }
 }
 
+const boxAppear = (currentBox) => {
+
+    currentBox.style.zIndex = '100';
+    currentBox.style.opacity = '1';
+}
+
+const boxFade = (currentBox) => {
+
+    currentBox.addEventListener('mouseleave', () => {
+        currentBox.style.transition = '.2s';
+        currentBox.style.zIndex = '0';
+        currentBox.style.opacity = '0';
+
+    })
+
+}
+
+
+
 slider();
 
+previous.addEventListener('mouseover', () => {
 
+    boxInter.style.opacity = '0';
+    boxInter.style.zIndex = '0';
+    boxGrav.style.opacity = '0';
+    boxGrav.style.zIndex = '0';
+})
 previous.addEventListener('click', () => {
+
 
     if (contador === 1) {
         return;
     }
     else {
+
         contador--;
         slider();
     }
 
 })
 
+next.addEventListener('mouseover', () => {
+    boxMartian.style.opacity = '0';
+    boxMartian.style.zIndex = '0';
+    boxInter.style.opacity = '0';
+    boxInter.style.zIndex = '0';
+
+})
 next.addEventListener('click', () => {
+
     if (contador === 3) {
         return
     }
     else {
+
         contador++;
         slider();
+
     }
 })
-
 
 
 current.addEventListener('mouseover', (e) => {
 
-    box.style.opacity = '1';
-    box.style.zIndex = '100';
-
-    if (currentNumber.value == 0) {
-
-        less.style.opacity = '.1';
-        less.style.cursor = 'cursor';
-
-    }
-
     if (e.target.id === 'img1') {
 
-         currentNumber.innerHTML = stickerMR;
-
-        more.addEventListener('click', () => {
-            
-            less.style.opacity = '1';
-            less.style.cursor = 'pointer';
-            stickerMR ++;
-            console.log(stickerMR);
-            
-        })
-
-        
-        
-        p.textContent = 'Consigue tus stickers de misión rescate';
-
+        boxAppear(boxMartian);
+        boxFade(boxMartian);
 
     }
+
     else if (e.target.id === 'img2') {
 
-        currentNumber.value = stickerIn;
-        p.textContent = 'Consigue tus stickers de Interstellar';
-        stickerIn = currentNumber.value;
+        boxAppear(boxInter);
+        boxFade(boxInter);
+
+
     }
-    else {
-        currentNumber.value = stickerGrav;
-        p.textContent = 'Consigue tus stickers de Gravity';
-        stickerGrav = currentNumber.value;
+    else if (e.target.id === 'img3') {
+
+        boxAppear(boxGrav);
+        boxFade(boxGrav);
+    }
+})
+
+
+/*----------------------Desafio---------------------------*/
+
+const input1 = document.getElementById('input1');
+
+const input2 = document.getElementById('input2');
+
+const input3 = document.getElementById('input3');
+
+
+
+let sticker1 = 0;
+let sticker2 = 0;
+let sticker3 = 0;
+
+let integ;
+
+let suma3Stickers;
+
+const sumaGlobal = () => {
+    suma3Stickers = Number(input1.value) + Number(input2.value) + Number(input3.value);
+
+}
+
+const topeStickers = () => {
+
+    if (suma3Stickers < 10) {
+
+        alertaM.textContent = `Total stickers: ${suma3Stickers}`;
+        alertaI.textContent = `Total stickers: ${suma3Stickers}`;
+        alertaG.textContent = `Total stickers: ${suma3Stickers}`;
+    } else {
+
+        alertaM.textContent = "Llevas muchos stickers!";
+        alertaI.textContent = "Llevas muchos stickers!";
+        alertaG.textContent = "Llevas muchos stickers!";
     }
 
-})
+}
 
 
 
-current.addEventListener('mouseleave', (e) => {
+const sumarStickers = (botton, input, sticker) => {
 
-    box.style.opacity = '0';
-    box.style.zIndex = '0';
-})
+    botton.addEventListener('click', () => {
+        sticker = input.value;
+        sticker++;
+        input.value = sticker;
+        sumaGlobal();
+        topeStickers();
+
+    })
+}
+const restarStickers = (botton, input, sticker) => {
+    botton.addEventListener('click', () => {
+
+        if (input.value == 0) {
+            return;
+        }
+        else {
+            sticker = input.value;
+            sticker--;
+            input.value = sticker;
+            sumaGlobal();
+            topeStickers();
+        }
+    })
+}
+
+const valorInput = (input, sticker) => {
+    input.addEventListener('change', () => {
+        if (isNaN(input.value)) {
+            input.value = sticker
+        } else {
+            integ = Math.ceil(input.value);
+            sticker = integ;
+            input.value = sticker;
+            sumaGlobal();
+            topeStickers();
+        }
+
+    })
+}
+
+valorInput(input1, sticker1);
+valorInput(input2, sticker2);
+valorInput(input3, sticker3);
+
+sumarStickers(moreM, input1, sticker1);
+restarStickers(lessM, input1, sticker1);
+
+sumarStickers(moreI, input2, sticker2);
+restarStickers(lessI, input2, sticker2);
+
+sumarStickers(moreG, input3, sticker3);
+restarStickers(lessG, input3, sticker3);
 
 
 
-box.addEventListener('mouseover', e => {
-    box.style.transition = '.5s';
-    box.style.opacity = '1';
-    box.style.zIndex = '100';
-})
 
-box.addEventListener('mouseleave', e => {
-    box.style.opacity = '0';
-    box.style.zIndex = '0';
-})
+
+
+
 
 
 
